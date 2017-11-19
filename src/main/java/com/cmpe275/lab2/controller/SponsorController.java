13c8609bfb034359594f960038f2aa9e1a84c608
aaditya deowanshi <aadityadeowanshi@hotmail.com>
2017-11-18 22:20:59 -0800

    Updated Sponsor controller

--- src/main/java/com/cmpe275/lab2/controller/SponsorController.java
+++ src/main/java/com/cmpe275/lab2/controller/SponsorController.java
@@ -1,8 +1,12 @@
 package com.cmpe275.lab2.controller;
 
+import com.cmpe275.lab2.model.Player;
 import com.cmpe275.lab2.model.Sponsor;
+import com.cmpe275.lab2.service.PlayerService;
 import com.cmpe275.lab2.service.SponsorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,24 +15,29 @@ public class SponsorController {
 
 	@Autowired
 	private SponsorService sponsorService;
+	@Autowired
+    private PlayerService playerService;
 
 	@PostMapping(value = "/sponsor")
-	public @ResponseBody
-	Sponsor newPlayer(@RequestParam(value = "name", required = true) String name,
-					  @RequestParam(value = "description", required = false) String description,
-					  @RequestParam(value = "street", required = false) String street,
-					  @RequestParam(value = "city", required = false) String city,
-					  @RequestParam(value = "state", required = false) String state,
-					  @RequestParam(value = "zip", required = false) String zip) {
-		Sponsor sponsor = new Sponsor(name, description, street, city, state, zip);
-		sponsorService.addSponsor(sponsor);
+	public ResponseEntity newPlayer(@RequestParam(value = "name", required = true) String name,
+									@RequestParam(value = "description", required = false) String description,
+									@RequestParam(value = "street", required = false) String street,
+									@RequestParam(value = "city", required = false) String city,
+									@RequestParam(value = "state", required = false) String state,
+									@RequestParam(value = "zip", required = false) String zip) {
+		try {
+			Sponsor sponsor = new Sponsor(name, description, street, city, state, zip);
+			sponsorService.addSponsor(sponsor);
 
-		return sponsor;
+			return ResponseEntity.ok(sponsor);
+
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("400 Bad Request");
+		}
 	}
 
 	@PatchMapping(value = "/sponsor/{id}")
-	public @ResponseBody
-    Sponsor updateSponsor(@PathVariable("id") long SponsorId,
+	public ResponseEntity updateSponsor(@PathVariable("id") long SponsorId,
                           @RequestParam(value = "name", required = false) String name,
                           @RequestParam(value = "description", required = false) String description,
                           @RequestParam(value = "street", required = false) String street,
@@ -36,50 +45,52 @@ public class SponsorController {
                           @RequestParam(value = "state", required = false) String state,
                           @RequestParam(value = "zip", required = false) String zip) {
 
-		Sponsor sponsor = sponsorService.getSponsor(SponsorId);
+        try {
+            Sponsor sponsor = sponsorService.getSponsor(SponsorId);
 
-		if (sponsor != null) {
-			if (name != null) {
-				sponsor.setName(name);
-			}
-			if (description != null) {
-				sponsor.setDescription(description);
-			}
-			if (street != null) {
-				sponsor.setStreet(street);
-			}
-			if (city != null) {
-				sponsor.setCity(city);
+            sponsor.setName(name);
+            sponsor.setDescription(description);
+            sponsor.setStreet(street);
+            sponsor.setCity(city);
+            sponsor.setState(state);
+            sponsor.setZip(zip);
 
-			}if (state != null) {
-				sponsor.setState(state);
+            return  (sponsor != null) ?
+                    ResponseEntity.ok(sponsorService.updateSponsor(sponsor)) :
+                    ResponseEntity.status(HttpStatus.NOT_FOUND).body("404 Not Found");
 
-			}if (zip != null) {
-				sponsor.setZip(zip);
-
-			}
-
-			return sponsorService.updateSponsor(sponsor);
-		}
-
-		return null;
+        }catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("404 Not Found");
+        }
 	}
 
 	@GetMapping(value = "/sponsor/{id}")
-	public @ResponseBody
-    Sponsor getSponsorDetails(@PathVariable("id") long SponsorId) {
+	public ResponseEntity getSponsorDetails(@PathVariable("id") long SponsorId) {
+        try {
+            Sponsor sponsor = sponsorService.getSponsor(SponsorId);
 
-		Sponsor sponsor = sponsorService.getSponsor(SponsorId);
-
-		return sponsor;
+            return  (sponsor == null) ?
+                    ResponseEntity.status(HttpStatus.NOT_FOUND).body("404 Not Found"):
+                    ResponseEntity.ok(sponsor.toString());
+        }catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("404 Not Found");
+        }
 	}
 
 	@DeleteMapping(value = "/sponsor/{id}")
-	public @ResponseBody
-    String deleteSponsor(@PathVariable("id") long SponsorId) {
+	public ResponseEntity deleteSponsor(@PathVariable("id") long SponsorId) {
+        try {
+            Sponsor sponsor = sponsorService.getSponsor(SponsorId);
 
-	    sponsorService.deleteSponsor(SponsorId);
+            for(Player player:sponsor.getPlayers()) {
+                player.setSponsor(null);
+                playerService.updatePlayer(player);
+            }
+            sponsorService.deleteSponsor(SponsorId);
 
-		return "Deleted Successfully!";
+            return ResponseEntity.ok(sponsor);
+        }catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("404 Not Found");
+        }
 	}
 }
